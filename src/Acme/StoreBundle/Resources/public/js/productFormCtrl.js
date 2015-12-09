89b9d9ecb489e2c205be0816615f7718e94b284f
AndreyGot <andreygot88@gmail.com>
2015-12-09 21:51:41 +0200

    ng-repeat

--- src/Acme/StoreBundle/Resources/public/js/productFormCtrl.js
+++ src/Acme/StoreBundle/Resources/public/js/productFormCtrl.js
@@ -1,6 +1,8 @@
-productFormCtrl = function ($scope,$http) {
-	$http.get('/app_dev.php/category').then(function (response) {
-		console.log(response.data);
+productFormCtrl = function ($scope,$http)
+{
+	$http.get('/app_dev.php/category').then(function (response) 
+	{
+		$scope.categories = response.data;
 	});
 }
 angular.module('andrey').controller('productFormCtrl',productFormCtrl);
