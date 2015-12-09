1f5187b8d9e249a811994033ea8fa83fdc1da30f
AndreyGot <andreygot88@gmail.com>
2015-12-09 22:34:47 +0200

    add bootstrap

--- src/Acme/StoreBundle/Resources/public/js/andreyApp.js
+++ src/Acme/StoreBundle/Resources/public/js/andreyApp.js
@@ -1,4 +1,4 @@
-angular.module('andrey',['ui.router'])
+angular.module('andrey',['ui.router','ui.bootstrap'])
   .config(['$interpolateProvider', function ($interpolateProvider) {
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
